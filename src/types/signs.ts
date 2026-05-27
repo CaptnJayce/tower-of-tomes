c9a23ddda64abf747ee07c44e0711affb86bcd7c
CaptnJayce <captn.dev@protonmail.com>
2026-05-27 23:57:16 +0100

    feat: add sign assets and type definitions

---
+++ src/types/signs.ts
@@ -0,0 +1,123 @@
+import bendSvg from "../assets/signs/Bend.svg";
+import billowingSvg from "../assets/signs/Billowing.svg";
+import birdSvg from "../assets/signs/Bird.svg";
+import boltSvg from "../assets/signs/Bolt.svg";
+import collectionSvg from "../assets/signs/Collection.svg";
+import columnSvg from "../assets/signs/Column.svg";
+import convergenceSvg from "../assets/signs/Convergence.svg";
+import crosshairSvg from "../assets/signs/Crosshair.svg";
+import crushSvg from "../assets/signs/Crush.svg";
+import dancingPuppetSvg from "../assets/signs/Dancing_Puppet.svg";
+import diamondSvg from "../assets/signs/Diamond.svg";
+import directionSvg from "../assets/signs/Direction.svg";
+import dispersionSvg from "../assets/signs/Dispersion.svg";
+import enlargeSvg from "../assets/signs/Enlarge.svg";
+import eyeSvg from "../assets/signs/Eye.svg";
+import floatSvg from "../assets/signs/Float.svg";
+import levitationSvg from "../assets/signs/Levitation.svg";
+import pullSvg from "../assets/signs/Pull.svg";
+import radialSvg from "../assets/signs/Radial.svg";
+import rainSvg from "../assets/signs/Rain.svg";
+import repetitionSvg from "../assets/signs/Repetition.svg";
+import visionSvg from "../assets/signs/Vision.svg";
+import weaveSvg from "../assets/signs/Weave.svg";
+import windowSvg from "../assets/signs/Window.svg";
+
+export const SIGNS = [
+  {
+    name: "Bend",
+    image: bendSvg,
+  },
+  {
+    name: "Billowing",
+    image: billowingSvg,
+  },
+  {
+    name: "Bird",
+    image: birdSvg,
+  },
+  {
+    name: "Bolt",
+    image: boltSvg,
+  },
+  {
+    name: "Collection",
+    image: collectionSvg,
+  },
+  {
+    name: "Column",
+    image: columnSvg,
+  },
+  {
+    name: "Convergence",
+    image: convergenceSvg,
+  },
+  {
+    name: "Crosshair",
+    image: crosshairSvg,
+  },
+  {
+    name: "Crush",
+    image: crushSvg,
+  },
+  {
+    name: "Dancing Puppet",
+    image: dancingPuppetSvg,
+  },
+  {
+    name: "Diamond",
+    image: diamondSvg,
+  },
+  {
+    name: "Direction",
+    image: directionSvg,
+  },
+  {
+    name: "Dispersion",
+    image: dispersionSvg,
+  },
+  {
+    name: "Enlarge",
+    image: enlargeSvg,
+  },
+  {
+    name: "Eye",
+    image: eyeSvg,
+  },
+  {
+    name: "Float",
+    image: floatSvg,
+  },
+  {
+    name: "Levitation",
+    image: levitationSvg,
+  },
+  {
+    name: "Pull",
+    image: pullSvg,
+  },
+  {
+    name: "Radial",
+    image: radialSvg,
+  },
+  {
+    name: "Rain",
+    image: rainSvg,
+  },
+  {
+    name: "Repetition",
+    image: repetitionSvg,
+  },
+  {
+    name: "Vision",
+    image: visionSvg,
+  },
+  {
+    name: "Weave",
+    image: weaveSvg,
+  },
+  {
+    name: "Window",
+    image: windowSvg,
+  },
+];
